cd4ec8808784496d547e6378eb447ca0fc260166
Jardel <jardel1101@gmail.com>
2021-03-22 16:38:10 -0300

    Insert transaction in api

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,9 +1,10 @@
-import { useState } from 'react'
+import { FormEvent, useState } from 'react'
 import Modal from 'react-modal'
 
 import closeSvg from '../../assets/close.svg'
 import incomeSvg from '../../assets/income.svg'
 import outcomeSvg from '../../assets/outcome.svg'
+import api from '../../services/api'
 
 import { Container, TransactionsTypeContainer, RadioBox } from './styles'
 
@@ -13,7 +14,19 @@ interface INewTransactionModal {
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: INewTransactionModal) {
+  const [transaction, setTransaction] = useState({
+    title: '',
+    value: 0,
+    category: '',
+    type: 'deposit'
+  })
   const [type, setType] = useState('deposit')
+
+  function handleCreateNewTransaction(event: FormEvent) {
+    event.preventDefault();
+    //console.log(transaction)
+    api.post('/transactions', transaction)
+  }
 
   return (
 
@@ -28,12 +41,21 @@ export function NewTransactionModal({ isOpen, onRequestClose }: INewTransactionM
         <img src={closeSvg} alt="Fechar modal" />
       </button>
       
-      <Container>
+      <Container onSubmit={ handleCreateNewTransaction }>
         <h2>Cadastrar transação</h2>
         
-        <input placeholder="Título" />
+        <input
+          placeholder="Título"
+          value={transaction.title}
+          onChange={e => setTransaction({...transaction, title: e.target.value})}
+        />
 
-        <input placeholder="Valor" type="number" />
+        <input
+          placeholder="Valor"
+          type="number"
+          value={transaction.value}
+          onChange={e => setTransaction({...transaction, value: Number(e.target.value)})}
+        />
 
         <TransactionsTypeContainer>
           <RadioBox
@@ -57,7 +79,11 @@ export function NewTransactionModal({ isOpen, onRequestClose }: INewTransactionM
           </RadioBox>
         </TransactionsTypeContainer>
 
-        <input placeholder="Categoria" />
+        <input
+          placeholder="Categoria"
+          value={transaction.category}
+          onChange={e => setTransaction({...transaction, category: e.target.value})}  
+        />
 
         <button type='submit'>Cadastrar</button>
       </Container>
--- src/index.tsx
+++ src/index.tsx
@@ -1,46 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createServer } from 'miragejs'
+import { createServer, Model } from 'miragejs'
 
 import { App } from './App';
 
 createServer({
+  models: {
+    transaction: Model,
+  },
+
   routes() {
-     this.get('/transactions', () => {
-      return [
-        {
-          id: 'uuid-a1',
-          title: 'Pagamento financiamento',
-          amount: 600,
-          type: 'withdraw',
-          category: 'Fixas mensais',
-          created_at: new Date()
-        },
-        {
-          id: 'uuid-a2',
-          title: 'Salário',
-          amount: 3000,
-          type: 'deposit',
-          category: 'Trabalho',
-          created_at: new Date()
-        },
-        {
-          id: 'uuid-a3',
-          title: 'Verda carro',
-          amount: 10000,
-          type: 'deposit',
-          category: 'extras',
-          created_at: new Date()
-        },
-        {
-          id: 'uuid-a4',
-          title: 'Pagamento agua e luz',
-          amount: 400,
-          type: 'withdraw',
-          category: 'Fixas mensais',
-          created_at: new Date()
-        },
-      ]
+    this.namespace = 'api'
+    
+    this.get('/transactions', () => {
+      return this.schema.all('transaction')
+    })
+
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody)
+
+      return data
+      //return schema.create('transaction', data)
     })
   }
 })
